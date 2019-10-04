dc37b2608999a101e0ddad28f27ca8e8a8409479
Evgeniy Kovalev <rabota142@inbox.ru>
2019-10-04 08:08:00 +0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace CheckProxy
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnDnl = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.btnStart, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.btnDnl, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 1, 1);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(210, 62);
+            this.tableLayoutPanel1.TabIndex = 5;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnStart.Location = new System.Drawing.Point(3, 34);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(99, 25);
+            this.btnStart.TabIndex = 6;
+            this.btnStart.Text = "Начать";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnDnl
+            // 
+            this.btnDnl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnDnl.Location = new System.Drawing.Point(3, 3);
+            this.btnDnl.Name = "btnDnl";
+            this.btnDnl.Size = new System.Drawing.Size(99, 25);
+            this.btnDnl.TabIndex = 1;
+            this.btnDnl.Text = "Загрузить";
+            this.btnDnl.UseVisualStyleBackColor = true;
+            this.btnDnl.Click += new System.EventHandler(this.btnDnl_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Location = new System.Drawing.Point(108, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 31);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Count[0]";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label2.Location = new System.Drawing.Point(108, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(99, 31);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Остановлено";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(238, 15);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Успешно: ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(238, 46);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Плохие: ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(298, 15);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(13, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "0";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(298, 46);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(13, 13);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(321, 78);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "CheckProxy";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnDnl;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Leaf.xNet;
+
+namespace CheckProxy
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        List<string> proxy { get; set; } = new List<string>() { };
+        string Link = "https://www.google.ru/";
+        string writePath = @"C:\Users\Евгений\source\repos\CheckProxy\CheckProxy\ClearProxy.txt";
+        string pathFile = @"C:\Users\Евгений\source\repos\CheckProxy\CheckProxy\Proxy.txt";
+
+        private void btnDnl_Click(object sender, EventArgs e)
+        {
+            proxy = File.ReadAllLines(pathFile).ToList();
+            label1.Text = Convert.ToString(proxy.Count);
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            label2.Text = "В процессе";
+            for (int i = 0; i < 2100; i++)
+            {
+                string address = proxy[i].ToString().Replace(" ", "");
+                if (Get(Link, address))
+                {
+                    using (StreamWriter sw = new StreamWriter(writePath, true, Encoding.Default))
+                    {
+                        sw.Write(Environment.NewLine + proxy[i].ToString().Replace(" ", ""));
+                    }
+                }
+                proxy.RemoveAt(i);
+            }
+
+            for (int i = 0; i < proxy.Count; i++)
+            {
+                using (StreamWriter sw = new StreamWriter(pathFile, false, Encoding.Default))
+                {
+                    sw.Write(Environment.NewLine + proxy[i].ToString());
+                }
+            }
+            label2.Text = "Завершено!";
+        }
+
+        int bad = 0;
+        int good = 0;
+
+        public bool Get(string Link, string address)
+        {
+            HttpRequest request = new HttpRequest
+            {
+                KeepAlive = true
+            };
+            request.ConnectTimeout = 2000;
+            request.UserAgentRandomize();
+            request.Proxy = ProxyClient.Parse(ProxyType.HTTP, address);
+            request.Proxy.ReadWriteTimeout = 2000;
+            request.ReadWriteTimeout = 2000;
+            request.Proxy.ConnectTimeout = 2000;
+            bool answer = false;
+
+            try
+            {
+                request.Get(Link).ToString();
+                label5.Text = good++.ToString();
+                answer = true;
+            }
+            catch
+            {
+
+                label6.Text = bad++.ToString();
+                answer = false;
+            }
+            return answer;
+        }
+    }     
+}
